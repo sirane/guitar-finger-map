f234eda2d9711b84cd9351ca3bc53703b6dc3a66
Mofsuke <sirane7@gmail.com>
2024-06-23 03:09:48 +0900

    もっとリニューアル

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 
 // フォーカスされている音名indexを格納する。
 let focusNoteIndex = 7;
+const inner_html_for_degree_index = [
+  "<note-root>R</note-root>",
+  "<note-9th>m9</note-9th>",
+  "<note-9th>M9</note-9th>",
+  "<note-3rd>m3</note-3rd>",
+  "<note-3rd>M3</note-3rd>",
+  "<note-11th></note-11th>",
+  "<note-11th>♭5</note-11th>",
+  "<note-5th>5</note-5th>",
+  "<note-13th>13</note-13th>",
+  "<note-13th></note-13th>",
+  "<note-7th>m7</note-7th>",
+  "<note-7th>M7</note-7th>",
+];
 // 12音名をAからGまで配列に格納する。
-const notes = ["A", "As", "B", "C", "Cs", "D", "Ds", "E", "F", "Fs", "G", "Gs"];
+const inner_html_for_note_index = [
+  "A",
+  "A#",
+  "B",
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#"
+];
 // 12音名ごとの色を配列に格納する。シャープやフレットは真っ白で、他の色はそれぞれ違った薄い色を採用する。
 const sound_colors = [
   "#ffffff", // 1
@@ -20,15 +47,15 @@ const sound_colors = [
 ];
 // 各種スケールの設定
 const scales = [
-  {"indices":[0, 2, 4, 5, 7, 9, 11], "penta": [0, 2, 4, 7, 9] ,"label":"Ion(Maj)", "bgcolor":"#000"},
-  {"indices":[0, 2, 3, 5, 7, 8, 10], "penta": [0, 3, 5, 7, 10] ,"label":"Aeo(min)", "bgcolor":"#000"},
-  {"indices":[0, 2, 3, 5, 7, 9, 10], "penta": [0, 3, 5, 7, 10] ,"label":"Dori", "bgcolor":"#000"},
-  {"indices":[0, 1, 3, 5, 7, 8, 10], "penta": [0, 3, 5, 7, 10] ,"label":"Phry", "bgcolor":"#000"},
-  {"indices":[0, 2, 4, 6, 7, 9, 11], "penta": [0, 2, 4, 7, 9] ,"label":"Lydi", "bgcolor":"#000"},
-  {"indices":[0, 2, 4, 5, 7, 9, 10], "penta": [0, 2, 4, 7, 9] ,"label":"Mixo", "bgcolor":"#000"},
-  {"indices":[0, 1, 3, 5, 6, 8, 10], "penta": [0, 3, 5, 6, 10] ,"label":"Loca", "bgcolor":"#000"},
+  {"indices":[0, 2, 4, 5, 7, 9, 11], "penta": [0, 2, 4, 7, 9] ,"label":"Ionian(Maj)", "bgcolor":"#000"},
+  {"indices":[0, 2, 3, 5, 7, 8, 10], "penta": [0, 3, 5, 7, 10] ,"label":"Aeolian(Min)", "bgcolor":"#000"},
+  {"indices":[0, 2, 3, 5, 7, 9, 10], "penta": [0, 3, 5, 7, 10] ,"label":"Dorian", "bgcolor":"#000"},
+  {"indices":[0, 1, 3, 5, 7, 8, 10], "penta": [0, 3, 5, 7, 10] ,"label":"Phrygian", "bgcolor":"#000"},
+  {"indices":[0, 2, 4, 6, 7, 9, 11], "penta": [0, 2, 4, 7, 9] ,"label":"Lydian", "bgcolor":"#000"},
+  {"indices":[0, 2, 4, 5, 7, 9, 10], "penta": [0, 2, 4, 7, 9] ,"label":"Mixolydian", "bgcolor":"#000"},
+  {"indices":[0, 1, 3, 5, 6, 8, 10], "penta": [0, 3, 5, 6, 10] ,"label":"Locrian", "bgcolor":"#000"},
   {"indices":[0, 1, 3, 4, 6, 8, 10], "penta": [0, 3, 4, 7, 10] ,"label":"Altered", "bgcolor":"#000"}, 
-  {"indices":[0, 2, 3, 5, 7, 9, 11], "penta": [0, 3, 5, 7, 9] ,"label":"Melo-m", "bgcolor":"#000"},
+  {"indices":[0, 2, 3, 5, 7, 9, 11], "penta": [0, 3, 5, 7, 9] ,"label":"Melodic Min", "bgcolor":"#000"},
 ]
 
 // 現在のスケール名を格納する
@@ -41,15 +68,20 @@ function switchScaleToNext(){
   currentScale = scales[currentScaleIndex];
   return currentScale
 }
-
+function switchScaleToPrev(){
+  currentScaleIndex = (currentScaleIndex - 1 + scales.length) % scales.length;
+  currentScale = scales[currentScaleIndex];
+  return currentScale
+}
 function update_control_appearance() {
   let scale_name_bgcolor = currentScale["bgcolor"];
   let scale_name_inner_html = currentScale["label"];
-  scale_name_inner_html = ` ${notes[focusNoteIndex]} ${scale_name_inner_html}`;
+  scale_name_inner_html = ` ${inner_html_for_note_index[focusNoteIndex]} ${scale_name_inner_html}`;
   document.getElementById("button-change-scale").style.backgroundColor =
     scale_name_bgcolor;
   document.getElementById("button-change-scale").innerHTML =
     scale_name_inner_html;
+  document.getElementById("scale-index").innerHTML = `${currentScaleIndex+1}/${scales.length}`;
   console.debug("scale_name_inner_html", scale_name_inner_html);
 }
 
@@ -57,6 +89,8 @@ const CSS_FRET_BORDER_WEIGHT = "5px";
 const CSS_FRET_BORDER_COLOR_FOCUSED = "#88d";
 // 0fretの音名(EBGDAE) をnotesのindexで配列に格納する。
 const openStrings = [7, 2, 10, 5, 0, 7];
+const focusDots = [3,5,7,9,15,17,19,21];
+const focusDoubleDots = [12];
 let is_display_degree_name = true;
 const DISPLAY_MODE_CODE = "code";
 const DISPLAY_MODE_PENTA = "penta";
@@ -90,27 +124,16 @@ function generateFretBoard() {
       create_new_cell(j, i, fret);
     }
     // フレットごとの処理
-    // 3,5,7,9,12,15,17,19,21フレットに点を表示する。
-    if (
-      i === 3 ||
-      i === 5 ||
-      i === 7 ||
-      i === 9 ||
-      i === 15 ||
-      i === 17 ||
-      i === 19 ||
-      i === 21
-    ) {
+    if ( focusDots.includes(i) ) {
       let dot = document.createElement("div");
       dot.className = "dot";
       fret.appendChild(dot);
-    } else if (i === 12) {
+    } else if (focusDoubleDots.includes(i)) {
       let dot = document.createElement("div");
       dot.className = "double-dot";
       fret.appendChild(dot);
     } else {
       let dot = document.createElement("div");
-      dot.className = "dot";
       fret.appendChild(dot);
       dot.style.visibility = "hidden";
     }
@@ -123,10 +146,10 @@ function generateFretBoard() {
 
     // まずセル属性を計上する --------------------------------------------
     // 6弦から1弦までの"j"fretの音名をnotesのindexで配列に格納する
-    let note_index = (openStrings[j] + i) % notes.length;
+    let note_index = (openStrings[j] + i) % inner_html_for_note_index.length;
     // focusNoteIndexからの相対度数を計算する。
     let relativeNoteIndex =
-      (note_index - focusNoteIndex + notes.length) % notes.length;
+      (note_index - focusNoteIndex + inner_html_for_note_index.length) % inner_html_for_note_index.length;
     // scaleごとのセル属性を決定
     let is_penta = false;
     let is_in_scale = false;
@@ -156,37 +179,23 @@ function generateFretBoard() {
     // focusNoteIndexからの相対度数を表示する。短3度,長三度,完全4度,増4度,完全5度,短7度,長7度にはm3,M3,P4,A4,P5,m7,M7を表示する
     note = "";
     if (is_display_degree_name) {
-      if (relativeNoteIndex === 0) {
-        note = "<note-root>R</note-root>";
-      } else if (relativeNoteIndex === 1) {
-        note = "<note-9th>m9</note-9th>";
-      } else if (relativeNoteIndex === 2) {
-        note = "<note-9th>M9</note-9th>";
-      } else if (relativeNoteIndex === 3) {
-        note = "<note-3rd>m3</note-3rd>";
-      } else if (relativeNoteIndex === 4) {
-        note = "<note-3rd>M3</note-3rd>";
-      } else if (relativeNoteIndex === 5) {
-        note = "<note-11th></note-11th>";
-      } else if (relativeNoteIndex === 6) {
-        note = "<note-11th>♭5</note-11th>";
-      } else if (relativeNoteIndex === 7) {
-        note = "<note-5th>5</note-5th>";
-      } else if (relativeNoteIndex === 8) {
-        note = "m6";
-      } else if (relativeNoteIndex === 9) {
-        note = "M6";
-      } else if (relativeNoteIndex === 10) {
-        note = "<note-7th>m7</note-7th>";
-      } else if (relativeNoteIndex === 11) {
-        note = "<note-7th>M7</note-7th>";
-      }
+      note = inner_html_for_degree_index[relativeNoteIndex%12];
       // ただし、j=5の場合はrelativeNoteIndex=0以外は表示しない。
-      if (j === 5 && relativeNoteIndex !== 0) {
-        note = "";
-      }
+      // if (j === 5 && relativeNoteIndex !== 0) {
+      //   note = "";
+      // }
     }else{
-      note = notes[note_index]
+      note = inner_html_for_note_index[note_index%12]
+      // ルート音の場合は強調
+      if ( relativeNoteIndex == 0) {
+        note = `<note-root>${note}</note-root>`;
+      }else if (relativeNoteIndex == 3){
+        note = `<note-3rd>${note}</note-3rd>`;
+      }else if (relativeNoteIndex == 4){
+        note = `<note-3rd>${note}</note-3rd>`;
+      }else if (relativeNoteIndex == 7){
+        note = `<note-5th>${note}</note-5th>`;
+      }
     }
     
     string.innerHTML = note;
